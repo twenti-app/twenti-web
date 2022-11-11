1cf3feeaefd01d382d39f55a6a90823c788f486f
Arrastia <arrastia@pm.me>
2022-11-11 23:48:04 +0100

    FIX - lint & prettier issues

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react"
-import App from "../App"
+import { render, screen } from '@testing-library/react';
 
-describe('App component', () => {    
-    it('should render sample text', () => {
-        render(<App />)
-        expect(screen.getByText('Some sample text for vitest')).toBeDefined()
-    })
-})
+import App from '../App';
+
+describe('App component', () => {
+  it('should render sample text', () => {
+    render(<App />);
+    expect(screen.getByText('Some sample text for vitest')).toBeDefined();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
