cfd77ca891971411fbeaf2d8fb076a6a3a52e3ee
Arrastia <arrastia@pm.me>
2022-11-21 19:22:26 +0100

    REFACTOR - pages & entry point

--- src/ui/pages/AccessRequest/AccessRequest.tsx
+++ src/ui/pages/AccessRequest/AccessRequest.tsx
@@ -8,20 +8,18 @@ import { LabelledInput } from 'ui/components/LabelledInput';
 import { Link } from 'ui/components/Link';
 import { Logo } from 'ui/components/Logo';
 
-export const AccessRequest = () => {
-  return (
-    <Styles.View>
-      <Logo className="logo" />
-      <Form title="Necesito invitaciones">
-        <Banner
-          text="Ahora puedes añadir tu e-mail a esta lista para que podamos comprobar si alguno de tus amigos está en Tuenti y puede enviarte una
+export const AccessRequest = () => (
+  <Styles.View>
+    <Logo className="logo" />
+    <Form title="Necesito invitaciones">
+      <Banner
+        text="Ahora puedes añadir tu e-mail a esta lista para que podamos comprobar si alguno de tus amigos está en Tuenti y puede enviarte una
           invitación."
-          textAlign="left"
-        />
-        <LabelledInput label="E-mail" name="email" type="email" />
-        <LabelledInput label="?" name="email" type="email" />
-      </Form>
-      <Link to={routes.LANDING}>Volver</Link>
-    </Styles.View>
-  );
-};
+        textAlign="left"
+      />
+      <LabelledInput label="E-mail" name="email" type="email" />
+      <LabelledInput label="?" name="email" type="email" />
+    </Form>
+    <Link to={routes.LANDING}>Volver</Link>
+  </Styles.View>
+);
