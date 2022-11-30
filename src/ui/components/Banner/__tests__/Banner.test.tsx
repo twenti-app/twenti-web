38606dacd577ea3fa52ba54babeafbef50702297
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-11-30 11:33:08 +0100

    TEST - should apply text align

--- src/ui/components/Banner/__tests__/Banner.test.tsx
+++ src/ui/components/Banner/__tests__/Banner.test.tsx
@@ -5,8 +5,21 @@ import { Banner } from '../Banner';
 describe('Banner tests', () => {
   const text = 'Sample text';
 
+  it('should render Banner component', () => {
+    render(<Banner text="" />);
+  });
+
   it('should render sample text', () => {
     render(<Banner text={text} />);
     expect(screen.getByText(text)).toBeDefined();
+  });
+
+  it('should apply text align', () => {
+    render(<Banner text={text} textAlign="center" />);
+
+    const expectedStyles = /text-align: center/;
+    const bannerStyles = screen.getByText(text).style.cssText;
+
+    expect(bannerStyles).toMatch(expectedStyles);
   });
 });
